997044c23a6adb2f185fb7bfc23087bf6fa6de2f
MadoScientist97 <sahilthehi3r7@gmail.com>
2023-07-12 00:39:09 +0530

    @Component pt 2

--- src/main/java/org/springfromscratch/Car.java
+++ src/main/java/org/springfromscratch/Car.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Car {
+    Car() {
+        System.out.println("Car is being instantiated with nothing!");
+    }
 }
--- src/main/java/org/springfromscratch/Main.java
+++ src/main/java/org/springfromscratch/Main.java
@@ -4,14 +4,34 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+
+        printHeading();
+
         var di = new AnnotationConfigApplicationContext("org.springfromscratch");
-        for (String s: di.getBeanDefinitionNames()) {
-            if (!s.contains("org.springframework")) {
-                var beanDef = di.getBeanDefinition(s);
-                System.out.println("Beans: " + s + "\nHas type of: " + beanDef.getBeanClassName());
-            }
-        }
+
         var car = di.getBean(Car.class);
         System.out.println("Car instance: " + car);
+
+        printBeanDefinition(di);
+    }
+
+    private static void printBeanDefinition(AnnotationConfigApplicationContext di) {
+        System.out.println("\n\n##################BeanDefinitions:######################");
+        for (String name: di.getBeanDefinitionNames()) {
+            if (name.contains("org.springframework"))
+                continue;
+
+            var beanDef = di.getBeanDefinition(name);
+            var type = beanDef.getBeanClassName();
+
+            System.out.println("Bean Definition:\n"+
+                    "Name: " + name + "\n" +
+                    "Type: " + type + "\n" +
+                    "-----------------------------------------");
+        }
+    }
+
+    private static void printHeading() {
+        System.out.println("========================Spring Application============================");
     }
 }
